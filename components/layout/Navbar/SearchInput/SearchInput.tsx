0b825dab831e268516b83265ba685a9a6e24579e
Kelechiodom10 <kelechiodom10@gmail.com>
2022-04-07 16:29:43 +0100

    Create reusable input for searching products

---
+++ components/layout/Navbar/SearchInput/SearchInput.tsx
@@ -0,0 +1,21 @@
+import CustomInput from "@components/shared/Input";
+import { Search } from "tabler-icons-react";
+import { useSearchInputStyles } from "./SearchInput.styles";
+
+const SearchInput: React.FC = () => {
+  const { classes } = useSearchInputStyles();
+  return (
+    <CustomInput
+      className={classes.inputContainer}
+      classNames={{
+        input: classes.input,
+        icon: classes.icon,
+      }}
+      icon={<Search color="black" />}
+      iconWidth={40}
+      placeholder="Search products"
+    />
+  );
+};
+
+export default SearchInput;
