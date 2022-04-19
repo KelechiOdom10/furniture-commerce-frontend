4824027d8d3f317b4898beb2ad268f186f1c772e
Kelechiodom10 <kelechiodom10@gmail.com>
2022-04-19 20:04:19 +0100

    Created product list component

---
+++ components/ProductList/index.tsx
@@ -0,0 +1,22 @@
+import ProductCard from "@components/ProductCard";
+import { Container, Grid } from "@mantine/core";
+import React from "react";
+import { ProductReadType } from "types";
+
+const ProductList: React.FC<{ products: ProductReadType[] }> = ({
+  products,
+}) => {
+  return (
+    <Container size={1400} style={{ paddingTop: 80, paddingBottom: 80 }}>
+      <Grid gutter={42} mx="auto" sx={{ width: "100%" }}>
+        {products.map(product => (
+          <Grid.Col key={product.id} span={12} sm={6} lg={4}>
+            <ProductCard product={product} />
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default ProductList;
