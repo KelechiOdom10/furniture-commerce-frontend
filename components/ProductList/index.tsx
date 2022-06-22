5671f39043e753e1a71ae4ed0fcfdd532bebbbd1
Kelechiodom10 <kelechiodom10@gmail.com>
2022-06-23 00:16:22 +0100

    Update wrapper with box component

--- components/ProductList/index.tsx
+++ components/ProductList/index.tsx
@@ -1,12 +1,12 @@
 import ProductCard from "@components/ProductCard";
-import { Container, Grid } from "@mantine/core";
+import { Box, Grid } from "@mantine/core";
 import { ProductReadType } from "types";
 
 const ProductList: React.FC<{ products: ProductReadType[] }> = ({
   products,
 }) => {
   return (
-    <Container size={1400} style={{ paddingTop: 80, paddingBottom: 80 }}>
+    <Box style={{ paddingTop: 45, paddingBottom: 80 }}>
       <Grid gutter={42} mx="auto" sx={{ width: "100%" }}>
         {products.map(product => (
           <Grid.Col key={product.id} span={12} sm={6} lg={4}>
@@ -14,7 +14,7 @@ const ProductList: React.FC<{ products: ProductReadType[] }> = ({
           </Grid.Col>
         ))}
       </Grid>
-    </Container>
+    </Box>
   );
 };
 
