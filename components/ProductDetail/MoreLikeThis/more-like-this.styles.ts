d8ae88bd8bc7fc37bc61af64e5481fa482036444
Kelechiodom10 <kelechiodom10@gmail.com>
2022-06-23 00:21:31 +0100

    Created more like this section shown in product detail page

---
+++ components/ProductDetail/MoreLikeThis/more-like-this.styles.ts
@@ -0,0 +1,15 @@
+import { createStyles } from "@mantine/core";
+
+export const useMoreLikeThisStyles = createStyles(() => ({
+  root: {
+    marginTop: 80,
+    marginBottom: 12,
+    width: "100%",
+  },
+  productsContainer: {
+    overflowX: "auto",
+    width: "100%",
+    marginTop: 24,
+    gap: 24,
+  },
+}));
