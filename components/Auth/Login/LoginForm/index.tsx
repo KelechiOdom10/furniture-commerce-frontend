595b57c5782132a76aaf196665a89ce0d28a2db6
Kelechiodom10 <kelechiodom10@gmail.com>
2022-07-05 19:39:01 +0100

    Created login form with custom validation

---
+++ components/Auth/Login/LoginForm/index.tsx
@@ -0,0 +1,61 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Stack, Title } from "@mantine/core";
+import { TextInput, PasswordInput } from "@components/shared/Input";
+import Button from "@components/shared/Button";
+import { UserLoginDto } from "types";
+
+const LoginForm: React.FC = () => {
+  const userLoginSchema = z.object({
+    email: z
+      .string()
+      .min(1, "Email is required")
+      .email("Invalid email address"),
+    password: z.string().min(1, "Password is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserLoginDto>({
+    resolver: zodResolver(userLoginSchema),
+  });
+  const handleLogin = handleSubmit(data => console.log(data));
+
+  return (
+    <Stack align="stretch">
+      <Title order={2}>Sign in</Title>
+      <form onSubmit={handleLogin} noValidate>
+        <Stack spacing={28}>
+          <TextInput
+            required
+            type="email"
+            label="Email Address"
+            placeholder="Enter valid email address"
+            error={errors?.email?.message}
+            {...register("email")}
+          />
+          <PasswordInput
+            required
+            size="md"
+            label="Password"
+            placeholder="Enter Password"
+            error={errors?.password?.message}
+            {...register("password")}
+          />
+          <Button
+            type="submit"
+            variant="light"
+            disabled={!!errors.email || !!errors.password}
+          >
+            Sign in
+          </Button>
+        </Stack>
+      </form>
+    </Stack>
+  );
+};
+
+export default LoginForm;
