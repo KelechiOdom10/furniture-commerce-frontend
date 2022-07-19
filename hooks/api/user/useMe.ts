b1ddf261bd4d7aa6cc41116300037ee4157eac21
Kelechiodom10 <kelechiodom10@gmail.com>
2022-07-19 20:21:51 +0100

    created custom api hook to get current logged in user

---
+++ hooks/api/user/useMe.ts
@@ -0,0 +1,18 @@
+import { UserReadType } from "types";
+import { AxiosError } from "axios";
+import { useQuery } from "react-query";
+import authService from "@services/authService";
+
+export const useMe = () => {
+  const queryInfo = useQuery<UserReadType, AxiosError>(
+    "me",
+    authService.getCurrentUser,
+    {
+      retry: 0,
+      // should be refetched in the background every 8 hours
+      staleTime: 1000 * 60 * 60 * 8,
+    }
+  );
+
+  return queryInfo;
+};
