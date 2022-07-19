e1b0f2ff7efc90c5f1eb447c79e4d95b9b568d64
Kelechiodom10 <kelechiodom10@gmail.com>
2022-07-19 20:12:12 +0100

    Added status codes constants

--- constants/index.ts
+++ constants/index.ts
@@ -12,3 +12,17 @@ export const QUERY_OPTIONS_DEFAULT: DefaultOptions = {
     refetchOnWindowFocus: true,
   },
 };
+
+export const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export const QUERY_KEYS = {};
